edc344482f641205b17ce8564bab810afad98efc
anydev1103 <90677735+anydev1103@users.noreply.github.com>
2021-10-23 14:14:41 +0900

    update by blongstomany
    cleanup code with blongstomany

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,11 +4,8 @@ const Message = require("./message");
 const Participant = require("./participant");
 
 // associations
-
-Conversation.hasMany(Participant);
-User.hasMany(Participant);
-Participant.belongsTo(User);
-Participant.belongsTo(Conversation);
+Conversation.belongsToMany(User, { through: Participant });
+User.belongsToMany(Conversation, { through: Participant });
 
 Conversation.hasMany(Message);
 Message.belongsTo(Conversation);
